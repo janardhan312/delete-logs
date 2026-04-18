4a572a3f608926ede47aa119fd2373900c53ae52
Janardhan <janardhancloud312@gmailcom>
2026-04-18 14:16:13 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -5,6 +5,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
@@ -27,3 +30,22 @@ USAGE(){
 if [ $# -lt 2 ]; then
     USAGE
 fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$R $SOURCE_DIR does not exist $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo -e "$R $DEST_DIR does not exist $N"
+    exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
+if [ -z "${FILES}" ]; then
+  echo "files found"
+else
+  echo -e "No files to do archeive .... $Y Skipping $N"
+fi
+
